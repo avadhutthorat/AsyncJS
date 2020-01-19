1064810dd94227b22adb31eb8b3e840d7f4f52fb
Avadhut Thorat <avadhutthorat40@gmail.com>
2020-01-19 10:27:17 +0530

    Added promise all and race

--- promises.js
+++ promises.js
@@ -32,3 +32,18 @@ const createPost = post => {
 createPost({ title: "post three", content: "This is post three" })
   .then(getPosts)
   .catch(err => console.log(err));
+
+// Promise.all
+const promise1 = Promise.resolve("Hello Guys");
+const promise2 = 8;
+const promise3 = new Promise((res, rej) => setTimeout(res, 2000, "GoodBye!!"));
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
+  res.json()
+);
+Promise.all([promise1, promise2, promise3, promise4])
+  .then(values => console.log(values))
+  .catch(err => console.log(err));
+
+Promise.race([promise1, promise2, promise3, promise4])
+  .then(values => console.log(values))
+  .catch(err => console.log(err));
